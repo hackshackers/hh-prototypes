fc721a82b74430c0e45146e937b71e255f619d0e
Josh Kadis <josh@alleyinteractive.com>
2016-11-11 11:33:57 -0500

    use relative path, url loader, and rebuild

--- prototype/webpack.config.js
+++ prototype/webpack.config.js
@@ -13,7 +13,7 @@ var webpackConfig = {
   ],
   output: {
     path: './dist',
-    publicPath: process.env.BUILD ? '' : '/assets/',
+    publicPath: '',
     filename: 'index_bundle.js'
   },
   module: {
@@ -26,28 +26,44 @@ var webpackConfig = {
         loaders: ['style', 'css', 'sass'],
         test: /\.scss$/
       },
+      {
+        test: /\.png$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          mimetype: 'image/png'
+        }
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          mimetype: 'image/jpg'
+        }
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       title: 'Template listing',
-      template: './src/assets/_wrapper.html'
+      template: './src/_wrapper.html'
     }),
     new HtmlWebpackPlugin({
       filename: 'homepage.html',
       title: 'Homepage',
-      template: './src/assets/_wrapper.html'
+      template: './src/_wrapper.html'
     }),
     new HtmlWebpackPlugin({
       filename: 'single.html',
       title: 'Single Page',
-      template: './src/assets/_wrapper.html'
+      template: './src/_wrapper.html'
     }),
     new HtmlWebpackPlugin({
       filename: 'styletile1.html',
       title: 'Style Tile',
-      template: './src/assets/_wrapper.html'
+      template: './src/_wrapper.html'
     })
   ]
 };
